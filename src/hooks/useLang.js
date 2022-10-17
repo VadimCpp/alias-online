5fe79da73f06b37f099434b7573aa555fb05ec8f
Vadym Kaninskyi <vadim.cpp@gmail.com>
2022-10-17 23:01:11 +0200

    #19 set default ru language

--- src/hooks/useLang.js
+++ src/hooks/useLang.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 const useLang = () => {
-  const [interfaceLang, setInterfaceLang] = useState("UA");
+  const [interfaceLang, setInterfaceLang] = useState("RU");
   const [learningLang, setLearningLang] = useState("EN");
   return {
     interfaceLang,
